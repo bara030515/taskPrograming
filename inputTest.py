a9bfa083fb3ed4faf712cce588efaff5fe1fb8c2
Seungha <shkimm03@naver.com>
2025-03-19 21:57:33 +0900

    과제 제출

---
+++ inputTest.py
@@ -0,0 +1,16 @@
+num1 = int (input ('숫자1 ==>'))
+num2 = int (input ('숫자2 ==>'))
+
+numSum = num1 + num2
+numSub = num1 - num2
+numMul = num1 * num2
+numDeb = num1 / num2
+numRem = num1 % num2
+numPow = num1 ** num2
+
+print (num1, '+', num2, '=', numSum)
+print (num1, '-', num2, '=', numSub)
+print (num1, '*', num2, '=', numMul)
+print (num1, '/', num2, '=', numDeb)
+print (num1, '%', num2, '=', numRem)
+print (num1, '**', num2, '=', numPow)
